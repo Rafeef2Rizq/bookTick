5faa501c8ac4de268ac577f70422f5c164e7abc0
Rafeef2Rizq <rfyfrzq7@gmail.com>
2025-10-25 08:10:55 +0200

    v3

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-        outDir: 'public/build', 
+     "builds": [
+    {
+      "src": "index.php",
+      "use": "@vercel/php"
     },
+    {
+      "src": "public/build/**/*",
+      "use": "@vercel/static"
+    }
+  ],
+  "outputDirectory": "public/build",
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
